81ee3ebb7cba8ee030041128d62d5902f3021fba
Gitthuma <george.githuma@gmail.com>
2021-05-22 13:41:40 +0300

    Set canvas width and height to be the same as those set on the css file for correct scaling

--- code/main.js
+++ code/main.js
@@ -4,3 +4,7 @@ const canvas = document.getElementById('canvas1');
 /*Reference ctx (short for context) using canvas variable and getContext 2d. This makes ctx an instance of canvas 2d api object and we can call all built-in canvas methods on the variable such as ctx fill rectangle to draw a rectangle*/
 const ctx = canvas.getContext('2d');
 
+/*Set canvas width and height to be the same as those set on the css file for correct scaling*/
+canvas.width = 800;
+canvas.height = 450;
+
