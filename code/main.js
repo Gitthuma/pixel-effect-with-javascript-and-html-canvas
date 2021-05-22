cc52d3f208994f43277c7fde8db1d054c340e490
Gitthuma <george.githuma@gmail.com>
2021-05-22 13:36:34 +0300

    Referenced ctx (short for context) using canvas variable and getContext 2d. This makes ctx an instance of canvas 2d api object and we can call all built-in canvas methods on the variable such as ctx fill rectangle to draw a rectangle

--- code/main.js
+++ code/main.js
@@ -1,3 +1,6 @@
 /*Reference canvas using getElementById*/
 const canvas = document.getElementById('canvas1');
 
+/*Reference ctx (short for context) using canvas variable and getContext 2d. This makes ctx an instance of canvas 2d api object and we can call all built-in canvas methods on the variable such as ctx fill rectangle to draw a rectangle*/
+const ctx = canvas.getContext('2d');
+
